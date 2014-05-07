b63cb0277bf0e748bb8b6c025ee61bb68eb72cd8
Jacob Bartel <bartel@cs.unc.edu>
2014-05-07 12:10:17 -0400

    Created composed recommender

--- src/groups/evolution/ComposedGroupEvolutionRecommender.java
+++ src/groups/evolution/ComposedGroupEvolutionRecommender.java
@@ -3,26 +3,36 @@ package groups.evolution;
 import groups.evolution.predictions.lists.PredictionListSelector;
 import groups.evolution.predictions.oldchoosers.OldGroupAndPredictionPair;
 import groups.seedless.SeedlessGroupRecommender;
-import groups.seedless.kelli.HybridCliqueMerger;
+import groups.seedless.SeedlessGroupRecommenderFactory;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.AbstractMap.SimpleEntry;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeSet;
 
 import org.jgrapht.UndirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.SimpleGraph;
 
-import bus.tools.TestingConstants;
-
-public class GroupEvolutionRecommenderImpl<V> implements GroupEvolutionRecommender<V> {
+public class ComposedGroupEvolutionRecommender<V> implements GroupEvolutionRecommender<V> {
 	
-	private SeedlessGroupRecommender<V> recommenderEngine;
+	private static final double DEFAULT_THRESHOLD_INCREMENT = 1.0;
 
+	private final SeedlessGroupRecommenderFactory<V> recommenderEngineFactory;
+	private final double thresholdIncrement;
+	
+	public ComposedGroupEvolutionRecommender(SeedlessGroupRecommenderFactory<V> recommenderEngineFactory) {
+		this.recommenderEngineFactory = recommenderEngineFactory;
+		this.thresholdIncrement = DEFAULT_THRESHOLD_INCREMENT;
+	}
+	
+	public ComposedGroupEvolutionRecommender(SeedlessGroupRecommenderFactory<V> recommenderEngineFactory, double thresholdIncrement) {
+		this.recommenderEngineFactory = recommenderEngineFactory;
+		this.thresholdIncrement = thresholdIncrement;
+	}
+	
 	@Override
 	public Collection<RecommendedEvolution<V>> generateRecommendations(
 			UndirectedGraph<V, DefaultEdge> oldSocialGraph,
@@ -79,26 +89,32 @@ public class GroupEvolutionRecommenderImpl<V> implements GroupEvolutionRecommend
 			Collection<Set<V>> oldGroups, Set<V> newMembers, Set<V> removedMembers,
 			Collection<Entry<V, V>> removedEdges, Collection<Entry<V, V>> addedEdges) {
 		
+		// Fix any null arguments
 		oldGroups = (oldGroups == null)? new TreeSet<Set<V>>() : oldGroups;
 		newMembers = (newMembers == null)? new TreeSet<V>() : newMembers;
 		removedMembers = (removedMembers == null)? new TreeSet<V>() : removedMembers;
 		removedEdges = (removedEdges == null)? new ArrayList<Entry<V,V>>() : removedEdges;
 		addedEdges = (addedEdges == null)? new ArrayList<Entry<V,V>>() : addedEdges;
 		
-		
 		//Tracks when an old group has been associated with a prediction
 		Collection<OldGroupAndPredictionPair<V>> usedPairings = new HashSet<OldGroupAndPredictionPair<V>>(); 	
-		
+
 		//Tracks which of the old groups have already been evolved
 		Collection<Set<V>> usedOldGroups = new HashSet<Set<V>>();
-		
+
 		//Tracks which of the predicted groups have been used to evolve old groups
 		Collection<Set<V>> usedRecommenderEngineResults = new HashSet<Set<V>>();
-		
+
 		//Keeps track of recommendations found across all thresholds
 		Collection<RecommendedEvolution<V>> allRecommendations = new ArrayList<RecommendedEvolution<V>>(); 
 		
-		recommenderEngine = new HybridCliqueMerger<V>(socialGraph);
+		
+		SeedlessGroupRecommender<V> recommenderEngine;
+		if (maximalCliques != null) {
+			recommenderEngine = recommenderEngineFactory.create(socialGraph, maximalCliques);
+		} else {
+			recommenderEngine = recommenderEngineFactory.create(socialGraph);
+		}
 		Collection<Set<V>> recommenderEngineResults = recommenderEngine.getRecommendations();
 		
 		int maxOldGroupSize = 0;
@@ -137,44 +153,10 @@ public class GroupEvolutionRecommenderImpl<V> implements GroupEvolutionRecommend
 			
 
 			round++;
-			threshold += TestingConstants.getThresholdIncrement();
+			threshold += thresholdIncrement;
 		}
 		
 		return allRecommendations;
 	}
-	
-	public static void main(String[] args) {
-		GroupMorphingModeler.selectExpectedScaling();
-		
-		UndirectedGraph<Integer, DefaultEdge> socialGraph = new SimpleGraph<Integer, DefaultEdge>(DefaultEdge.class);
-		socialGraph.addVertex(1);
-		socialGraph.addVertex(2);
-		socialGraph.addVertex(3);
-		socialGraph.addVertex(4);
-		socialGraph.addVertex(5);
-		socialGraph.addVertex(6);
-		socialGraph.addEdge(1, 2);
-		socialGraph.addEdge(1, 3);
-		socialGraph.addEdge(1, 4);
-		socialGraph.addEdge(1, 5);
-		socialGraph.addEdge(1, 6);
-		socialGraph.addEdge(2, 3);
-		socialGraph.addEdge(2, 4);
-		socialGraph.addEdge(2, 5);
-		socialGraph.addEdge(2, 6);
-		socialGraph.addEdge(3, 4);
-		Set<Integer> newMembers = new TreeSet<Integer>();
-		newMembers.add(5);
-		
-		Set<Integer> oldGroup = new TreeSet<Integer>();
-		oldGroup.add(1);
-		oldGroup.add(2);
-		oldGroup.add(3);
-		oldGroup.add(4);
-		Collection<Set<Integer>> oldGroups = new ArrayList<Set<Integer>>();
-		oldGroups.add(oldGroup);
-		
-		GroupEvolutionRecommender<Integer> recommender = new GroupEvolutionRecommenderImpl<Integer>();
-		System.out.println(recommender.generateRecommendations(socialGraph, oldGroups, newMembers, null, null, null));
-	}
+
 }
--- src/groups/evolution/GroupEvolutionRecommender.java
+++ src/groups/evolution/GroupEvolutionRecommender.java
@@ -10,7 +10,20 @@ import org.jgrapht.graph.DefaultEdge;
 public interface GroupEvolutionRecommender<V> {
 
 	public Collection<RecommendedEvolution<V>> generateRecommendations(
+			UndirectedGraph<V, DefaultEdge> oldSocialGraph,
+			UndirectedGraph<V, DefaultEdge> socialGraph, Collection<Set<V>> oldGroups);
+
+	public Collection<RecommendedEvolution<V>> generateRecommendations(
+			UndirectedGraph<V, DefaultEdge> oldSocialGraph, Collection<Set<V>> maximalCliques,
+			UndirectedGraph<V, DefaultEdge> socialGraph, Collection<Set<V>> oldGroups);
+
+	public Collection<RecommendedEvolution<V>> generateRecommendations(
 			UndirectedGraph<V, DefaultEdge> socialGraph, Collection<Set<V>> oldGroups,
-			Set<V> newMembers, Set<V> removedMembers,
-			Collection<Entry<V,V>> removedEdges, Collection<Entry<V,V>> addedEdges);
+			Set<V> newMembers, Set<V> removedMembers, Collection<Entry<V, V>> removedEdges,
+			Collection<Entry<V, V>> addedEdges);
+
+	public Collection<RecommendedEvolution<V>> generateRecommendations(
+			UndirectedGraph<V, DefaultEdge> socialGraph, Collection<Set<V>> maximalCliques,
+			Collection<Set<V>> oldGroups, Set<V> newMembers, Set<V> removedMembers,
+			Collection<Entry<V, V>> removedEdges, Collection<Entry<V, V>> addedEdges);
 }
