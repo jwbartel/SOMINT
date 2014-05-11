a36f107c8653a3f82a0c79ccf5227e54133021da
Jacob Bartel <bartel@cs.unc.edu>
2014-05-10 20:16:39 -0400

    Fixed inheritance tree for RecipientRecommender

--- src/recipients/RecipientRecommender.java
+++ src/recipients/RecipientRecommender.java
@@ -1,12 +1,16 @@
 package recipients;
 
+import general.actionbased.ActionBasedRecommender;
 import general.actionbased.CollaborativeAction;
 
 import java.util.Collection;
 
-public interface RecipientRecommender<V extends Comparable<V>> {
-	
-	public Collection<ScoredRecipientRecommendation<V>> recommendRecipients(CollaborativeAction<V> action);
-	
-	public Collection<ScoredRecipientRecommendation<V>> recommendRecipients(CollaborativeAction<V> action, int maxPredictions);
+public interface RecipientRecommender<V extends Comparable<V>> extends
+		ActionBasedRecommender<V> {
+
+	public Collection<ScoredRecipientRecommendation<V>> recommendRecipients(
+			CollaborativeAction<V> action);
+
+	public Collection<ScoredRecipientRecommendation<V>> recommendRecipients(
+			CollaborativeAction<V> action, int maxPredictions);
 }
--- src/recipients/groupbased/GroupBasedRecipientRecommender.java
+++ src/recipients/groupbased/GroupBasedRecipientRecommender.java
@@ -1,6 +1,5 @@
 package recipients.groupbased;
 
-import general.actionbased.ActionBasedRecommender;
 import general.actionbased.CollaborativeAction;
 
 import java.util.Collection;
@@ -9,7 +8,7 @@ import recipients.RecipientRecommender;
 import recipients.ScoredRecipientRecommendation;
 
 public interface GroupBasedRecipientRecommender<V extends Comparable<V>>
-		extends RecipientRecommender<V>, ActionBasedRecommender<V> {
+		extends RecipientRecommender<V> {
 
 	public Collection<ScoredRecipientRecommendation<V>> recommendRecipients(
 			CollaborativeAction<V> action);
